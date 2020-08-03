42630e26573ad33605b3d12952fee7a5a8a0652a
root <root@Kali>
2020-08-03 16:40:13 -0500

    added bot credentials and mirror messaging interface ...

--- AshBot/src/com/aeteams/AshBot/AshBot.java
+++ AshBot/src/com/aeteams/AshBot/AshBot.java
@@ -3,6 +3,10 @@ package com.aeteams.AshBot;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot ;
 import org.telegram.telegrambots.api.methods.send.SendMessage ;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class AshBot extends TelegramLongPollingBot {
 
@@ -22,15 +26,43 @@ public class AshBot extends TelegramLongPollingBot {
     @Override
     public void onUpdateReceived(Update update) {
 
+
+
         // ChatId
+
         long chatId = update.getMessage().getChatId() ;
 
         // UserText
+
         String userText = update.getMessage().getText() ;
 
         // BotOutput
 
         SendMessage botText = new SendMessage().setChatId(chatId).setText("userText") ;
+
+
+
+
+        // Finalizing Output of The Robot ...
+
+
+        try {
+
+            execute(botText) ;
+
+        }
+
+        catch (TelegramApiException apiException) {
+
+            apiException.printStackTrace();
+
+            Logger.getLogger(AshBot.class.getName()).log(Level.SEVERE , null , apiException);
+
+        }
+
+
+
+
 
     }
 
