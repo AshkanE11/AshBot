b5e62ffa0b5b7c012e7cc0ba7e201f9e6af01b0d
root <root@Kali>
2020-08-03 10:51:09 -0500

    changes in main and other things

--- AshBot/src/com/aeteams/AshBot/Main.java
+++ AshBot/src/com/aeteams/AshBot/Main.java
@@ -1,19 +1,24 @@
 package com.aeteams.AshBot;
 
 
-
-import org.
+import org.telegram.telegrambots.ApiContextInitializer ;
+import org.telegram.telegrambots.TelegramBotsApi ;
+import org.telegram.telegrambots.exceptions.TelegramApiException ;
+import org.telegram.telegrambots.exceptions.TelegramApiRequestException ;
 
 
 
 public class Main {
 
-    
+
     public static void main(String[] args) {
 
 
 
-	System.out.println("Started The project ...") ;
+	ApiContextInitializer.init() ;
+
+
+	TelegramBotsApi Bot = new TelegramBotsApi() ;
 
 
 
