0fcb4d72b508fbc495a99ac4f1965f73de55fb40
root <root@Kali>
2020-08-03 10:50:49 -0500

    added the AshBot.java File for completing the mirroring bot ...

---
+++ AshBot/src/com/aeteams/AshBot/AshBot.java
@@ -0,0 +1,39 @@
+package com.aeteams.AshBot;
+
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot ;
+import org.telegram.telegrambots.api.methods.send.SendMessage ;
+
+public class AshBot extends TelegramLongPollingBot {
+
+
+    @Override
+    public String getBotToken() {
+        return "1188286735:AAGLOSg8Czz8hRz9llt144ZhsaSg7Zm7q6E";
+    }
+
+
+    @Override
+    public String getBotUsername() {
+        return "AshkanEBot";
+    }
+
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+        // ChatId
+        long chatId = update.getMessage().getChatId() ;
+
+        // UserText
+        String userText = update.getMessage().getText() ;
+
+        // BotOutput
+
+        SendMessage botText = new SendMessage().setChatId(chatId).setText("userText") ;
+
+    }
+
+
+
+}
