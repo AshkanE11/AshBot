deaebd40ad3692a21f80015aaf6039ad4f1c8c37
root <root@Kali>
2020-08-09 15:06:51 -0500

    Updated KeyBoard Buttons And Applied %50 of the SMTP mailing commands and made the interface more user friendly ...

--- AshBot/src/com/aeteams/AshBot/AshBot.java
+++ AshBot/src/com/aeteams/AshBot/AshBot.java
@@ -19,6 +19,16 @@ import java.util.logging.Logger;
 public class AshBot extends TelegramLongPollingBot {
 
 
+
+
+    // Global Variables
+
+    String userSubhead , userMessage ;
+    SendMessage botText , botInstruct ;
+
+
+
+
     @Override
     public String getBotToken() {
         return "1188286735:AAGLOSg8Czz8hRz9llt144ZhsaSg7Zm7q6E";
@@ -36,17 +46,92 @@ public class AshBot extends TelegramLongPollingBot {
 
 
 
-        // ChatId
+        if (update.hasMessage() && update.getMessage().hasText()) {
 
-        long chatId = update.getMessage().getChatId() ;
 
-        // UserText
 
-        String userText = update.getMessage().getText() ;
+            if (update.getMessage().getText().equals("/start")) {
 
-        // BotOutput
 
-        SendMessage botWel = new SendMessage().setChatId(chatId).setText("Sooo ... What We Are Going To Do ?!?") ;
+
+
+
+
+                // ChatId
+
+                long chatId = update.getMessage().getChatId() ;
+
+                // UserText
+
+                String userText = update.getMessage().getText() ;
+
+                // BotOutput
+
+                botText = new SendMessage().setChatId(chatId).setText("Sooo ... What We Are Going To Do ?!? \n Tip: Use The KeyBoard Commands ...") ;
+
+
+
+
+
+
+
+
+
+            }
+
+
+
+            if (update.getMessage().getText().equals("/Mail")) {
+
+
+
+                // ChatId
+
+                long chatId = update.getMessage().getChatId() ;
+
+                // UserText
+
+                String userText = update.getMessage().getText() ;
+
+                // BotText
+
+                botText = new SendMessage().setChatId(chatId).setText("Ha Nice ! \n Started the Mailing Operation ...") ;
+
+
+
+
+
+                /* Bot Instructions &&& User Interfaces &&& Data Capturing */
+
+
+
+
+
+                // Get SubHeader
+
+                botInstruct = new SendMessage().setChatId(chatId).setText("Give Me Your Mail SubHead !!!") ;
+
+                userSubhead = update.getMessage().getText().toString() ;
+
+
+                // Get Users Text
+
+                botInstruct = new SendMessage().setChatId(chatId).setText("Listening ... \n Enter Your Message For Ashkan !!!") ;
+
+                userMessage = update.getMessage().getText().toString() ;
+
+
+
+
+            }
+
+
+
+
+
+        }
+
+
 
 
 
@@ -55,22 +140,29 @@ public class AshBot extends TelegramLongPollingBot {
         // Commands Reply
 
 
-        ReplyKeyboardMarkup keymark = new ReplyKeyboardMarkup() ;
-        List<KeyboardRow> rowlist = new ArrayList<>() ;
-        KeyboardRow keyrow1 = new KeyboardRow() ;
-        KeyboardRow keyrow2 = new KeyboardRow() ;
-        KeyboardRow keyrow3 = new KeyboardRow() ;
-        KeyboardRow keyrow4 = new KeyboardRow() ;
-        keyrow1.add("ONE") ;
-        keyrow2.add("TWO") ;
-        keyrow3.add("THREE") ;
-        keyrow4.add("FOUR") ;
-        rowlist.add(keyrow1) ;
-        rowlist.add(keyrow2) ;
-        rowlist.add(keyrow3) ;
-        rowlist.add(keyrow4) ;
-        keymark.setKeyboard(rowlist) ;
-        keymark.setResizeKeyboard(true) ;
+
+        ReplyKeyboardMarkup keyboard = new ReplyKeyboardMarkup() ;
+        List<KeyboardRow> keyrow = new ArrayList<>() ;
+        KeyboardRow Mail = new KeyboardRow() ;
+        KeyboardRow Overview = new KeyboardRow() ;
+        KeyboardRow History = new KeyboardRow() ;
+        KeyboardRow About = new KeyboardRow() ;
+        Mail.add("/Mail") ;
+        Overview.add("/Overview") ;
+        History.add("/History") ;
+        About.add("/About") ;
+        keyrow.add(Mail) ;
+        keyrow.add(Overview) ;
+        keyrow.add(History) ;
+        keyrow.add(About) ;
+        keyboard.setKeyboard(keyrow) ;
+        keyboard.setResizeKeyboard(true) ;
+        keyboard.setOneTimeKeyboard(false) ;
+        keyboard.setSelective(true) ;
+
+
+
+
 
 
 
@@ -83,7 +175,7 @@ public class AshBot extends TelegramLongPollingBot {
 
         try {
 
-            execute(botWel) ;
+            execute(botText) ;
 
         }
 
